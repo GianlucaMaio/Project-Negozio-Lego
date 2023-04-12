c8d161689e8238ae48d9a378885f166e0f55974a
GianlucaMaio <gianlucamaione26@gmail.com>
2023-04-12 11:55:25 +0200

    Create negozio_lego.sql

---
+++ negozio_lego.sql
@@ -0,0 +1,165 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Creato il: Apr 12, 2023 alle 11:53
+-- Versione del server: 10.4.27-MariaDB
+-- Versione PHP: 8.1.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `negozio_lego`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `catalogo`
+--
+
+CREATE TABLE `catalogo` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(100) NOT NULL,
+  `descrizione` varchar(255) NOT NULL,
+  `categoria` varchar(100) NOT NULL,
+  `immagine` longtext NOT NULL,
+  `prezzo` double NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `clienti`
+--
+
+CREATE TABLE `clienti` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `cognome` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `indirizzo` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `cap` varchar(5) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `localita` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `provincia` varchar(2) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `telefono_cellulare` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `codice_fiscale` varchar(16) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `email` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `ordini`
+--
+
+CREATE TABLE `ordini` (
+  `id` int(11) NOT NULL,
+  `data` date NOT NULL,
+  `importo` double NOT NULL,
+  `indirizzo` varchar(255) NOT NULL,
+  `cap` varchar(5) NOT NULL,
+  `localita` varchar(50) NOT NULL,
+  `provincia` varchar(2) NOT NULL,
+  `id_cliente` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `ordini_prodotti`
+--
+
+CREATE TABLE `ordini_prodotti` (
+  `id_ordine` int(11) NOT NULL,
+  `id_prodotto` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `profili`
+--
+
+CREATE TABLE `profili` (
+  `id` int(11) NOT NULL,
+  `username` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indici per le tabelle scaricate
+--
+
+--
+-- Indici per le tabelle `catalogo`
+--
+ALTER TABLE `catalogo`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indici per le tabelle `clienti`
+--
+ALTER TABLE `clienti`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indici per le tabelle `ordini`
+--
+ALTER TABLE `ordini`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `ordini` (`id_cliente`);
+
+--
+-- Indici per le tabelle `ordini_prodotti`
+--
+ALTER TABLE `ordini_prodotti`
+  ADD KEY `ordine` (`id_ordine`),
+  ADD KEY `prodotto` (`id_prodotto`);
+
+--
+-- Indici per le tabelle `profili`
+--
+ALTER TABLE `profili`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT per le tabelle scaricate
+--
+
+--
+-- AUTO_INCREMENT per la tabella `catalogo`
+--
+ALTER TABLE `catalogo`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT per la tabella `clienti`
+--
+ALTER TABLE `clienti`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT per la tabella `ordini`
+--
+ALTER TABLE `ordini`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT per la tabella `profili`
+--
+ALTER TABLE `profili`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
